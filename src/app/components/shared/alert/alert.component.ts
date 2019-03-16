89ca10bf1a27bf4e3f83e0e9fb24b6eb72a75042
Javier Gómez Luzón <javo14795@gmail.com>
2019-03-16 13:19:10 +0100

    AlertComponent 2.0.0
    Public function to put msg on the own component

--- src/app/components/shared/alert/alert.component.ts
+++ src/app/components/shared/alert/alert.component.ts
@@ -10,15 +10,53 @@ export class AlertComponent {
 
   constructor() { }
 
-  openTab(msg:any[]){
-    this.message = msg;
+  private openTab(){
     (document.querySelector('#navbarId') as HTMLElement).style.filter = "blur(6px)";
     (document.querySelector('#'+this.superId) as HTMLElement).style.filter = "blur(6px)";
     (document.querySelector("#openButton") as HTMLElement).click();
   }
 
-  focusOut(){
+  private focusOut(){
     (document.querySelector('#navbarId') as HTMLElement).style.filter = "blur(0px)";
     (document.querySelector('#'+this.superId) as HTMLElement).style.filter = "blur(0px)";
+  }
+
+  lostConnection(){
+    this.message = [
+      `Se ha perdido la conexión con el servidor.`,
+      "Por favor, revisa tu conexión a internet."
+    ];
+    this.openTab();
+  }
+
+  serverError(){
+    this.message = [
+      `Ha habido interno del servidor, vuelva a intentarlo 
+      más tarde.`
+    ];
+    this.openTab();
+  }
+
+  errorValidatingUser(){
+    this.message = [
+      `Ha habido un error validando los datos, vuelva a intentarlo
+      más tarde.`
+    ];
+    this.openTab();
+  }
+
+  emailAlreadyTaken(){
+    this.message = [
+      "El email con el que intenta registrarse ya está registrado"
+    ];
+    this.openTab();
+  }
+
+  verificationSent(){
+    this.message = [
+      "Su registro se ha casi completado, solo es necesario un paso más.",
+      "Verifique su correo mediante el enlace que se le ha enviado al mismo."
+    ];
+    this.openTab();
   }
 }
