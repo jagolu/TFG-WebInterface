df5989cb81e870e561bdcc657bcd36a00c0588be
Javier Gómez Luzón <javo14795@gmail.com>
2019-03-16 19:39:58 +0100

    Start emailVerification 1.0.0

--- src/app/components/logSign/email-verification/email-verification.component.ts
+++ src/app/components/logSign/email-verification/email-verification.component.ts
@@ -1,19 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { LoadingComponent } from '../../shared/loading/loading.component';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-email-verification',
   templateUrl: './email-verification.component.html',
   styles: []
 })
-export class EmailVerificationComponent{
+export class EmailVerificationComponent implements AfterViewInit{
+
+  @ViewChild(LoadingComponent) loading:LoadingComponent;
 
   private token:string;
+  private msg:string = "asdf"
 
-  constructor(private aR:ActivatedRoute) { 
-    this.token = this.aR.snapshot.paramMap.get('token');
-    this.aR.params.subscribe( params=>{
-      this.token = params["token"];
-    })
+  constructor(private aR:ActivatedRoute, private _authS:AuthenticationService) { 
+      this.token = this.aR.snapshot.paramMap.get('token');
+  }
+
+  ngAfterViewInit(){
+    this.checkEmail();
+  }
+
+  checkEmail(){
+    this.loading.startLoading();
+    this._authS.checkEmailValidation(this.token).subscribe(
+      success=>{
+        console.log("success");
+        this.loading.stopLoading();
+      },
+      error=>{
+        console.log("error");
+        this.loading.stopLoading();
+      }
+    )
   }
 }
