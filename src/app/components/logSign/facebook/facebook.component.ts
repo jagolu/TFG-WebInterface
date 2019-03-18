720bc48885e1447063c836082db04f93935114ab
Javier Gómez Luzón <javo14795@gmail.com>
2019-03-18 23:41:23 +0100

    Revert "Facebook & Google Component ---> SocialButtonComponent"
    This reverts commit 98689881007319d30e14d5cea8011aefe6695b59.

---
+++ src/app/components/logSign/facebook/facebook.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { AuthService, FacebookLoginProvider, SocialUser } from 'angularx-social-login';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
+
+
+@Component({
+  selector: 'app-facebook',
+  template: `
+  <div id="gSignInWrapper">
+    <div id="customBtn" class="customGPlusSignIn" (click)="signIn()">
+      <span class="icon"></span>
+      <span class="buttonText" [textContent]="msg"></span>
+    </div>
+  </div>`,
+  styleUrls: ['facebook.component.css']
+})
+
+export class FacebookComponent{
+
+  private msg:string = "Log in with Facebook";
+  private user:SocialUser;
+  private loggedIn: boolean;
+
+  constructor(private _authS:AuthService, private _authenticationS:AuthenticationService) { 
+    this._authS.authState.subscribe(user=>{
+      this.user = user;
+      this.loggedIn  = (user!=null);
+      this.msg = (this.loggedIn && user.provider=="FACEBOOK") ? 
+             `Continue as ${this.user.firstName}` : "Log in with Facebook";
+    })
+  }
+
+  signIn(){    
+    if(!this.loggedIn){
+      console.log("facebook");
+      this._authS.signIn(FacebookLoginProvider.PROVIDER_ID).catch(Error);
+      this._authenticationS.setUserFromSocialMedia("FACEBOOK");
+    }
+    else console.log("Ya estas logueado");
+  }
+
+}
