1f2431f249a2cad46a4016aea084f1a44b9dd8f3
Javier Gómez Luzón <javo14795@gmail.com>
2019-03-20 21:05:22 +0100

    Update loadingComponent --> loadingService

--- src/app/components/shared/loading/loading.component.ts
+++ src/app/components/shared/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
@@ -7,39 +7,6 @@ import { Component, Input, ElementRef } from '@angular/core';
 })
 export class LoadingComponent{
 
-  @Input() superId:string;
-  private mainH:string;
-  private col_height:string;
-
-  constructor(private eR:ElementRef) { 
-    this.eR.nativeElement.style.display ="none";
-  }
-
-  startLoading(){
-    this.saveWindowSize();
-    this.resize(true);
-  }
-
-  stopLoading(){
-    this.resize(false)
-  }
-
-  private saveWindowSize(){
-    this.mainH = (document.querySelector(".main") as HTMLElement).style.height;
-    this.col_height = this.eR.nativeElement.children[0].children[0].style.height;
-  }
-
-  private resize(hide:boolean){
-    (document.querySelector("#navbarId") as HTMLElement).style.display = hide? 
-        "none":"flex";
-    (document.querySelector("#"+this.superId) as HTMLElement).style.display = hide? 
-        "none":"block";
-    (document.querySelector(".main") as HTMLElement).style.height = hide ? 
-        window.innerHeight+"px" : this.mainH+"px";
-    this.eR.nativeElement.children[0].children[0].style.height = hide ?
-        window.innerHeight+"px" : this.col_height+"px";
-    this.eR.nativeElement.children[0].children[0].children[0].style.marginTop = hide?
-        "25%" : "0";
-    this.eR.nativeElement.style.display = hide ? "block" : "none";
-  }
+  constructor() { }
+  
 }
