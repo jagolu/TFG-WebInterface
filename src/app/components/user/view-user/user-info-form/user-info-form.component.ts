70b4ce08b064774bc840604ea74728f972851b89
Javier Gómez Luzón <javo14795@gmail.com>
2019-03-29 01:27:36 +0100

    Update UserInfo form
    Add all forms

--- src/app/components/user/view-user/user-info-form/user-info-form.component.ts
+++ src/app/components/user/view-user/user-info-form/user-info-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-info-form',
@@ -9,11 +10,77 @@ export class UserInfoFormComponent implements OnInit {
 
   @Input()id:string;
   @Input()labelled:string;
-  @Input()show:boolean;
+  @Input()nickname:string;
+
+  private nicknameForm:FormGroup;
+  private passwordForm:FormGroup;
+  private imageForm:FormGroup;
+  private equalPasswords : boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.initializeNicknameForm();
+    this.initializePasswordForm();
+    this.initializeImageForm();
+    this.equalPasswords = false;
+  }
+
+  private changeNickname(){
+    //TODO
+    console.log("Deberiamos cambiar el nickname");
+  }
+
+  private changePassword(){
+    //TODO
+    console.log(this.passwordForm);
+    console.log(this.equalPasswords)
+    console.log("Deberiamos cambiar la contraseña");
+  }
+
+  private changeImg(){
+    //TODO
+    console.log(this.imageForm.controls['userImage'])
+    console.log("Deberiamos cambiar la imagen de usuario");
+  }
+
+  private equalPassword(){
+    let password = this.passwordForm.controls['newPassword'].value;
+    let repeatPassword = this.passwordForm.controls['repeatPassword'].value;
+    this.equalPasswords = ((password == repeatPassword) && password.length>0 && repeatPassword.length>0);
+  }
+
+  private initializeNicknameForm(){
+    this.nicknameForm = new FormGroup({
+      'nickname': new FormControl(
+        this.nickname,[
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(20)
+        ]
+      )
+    })
+  }
+
+  private initializePasswordForm(){
+    let passValidators = [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(30),
+      Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+    ];
+
+    this.passwordForm = new FormGroup({
+      'oldPassword': new FormControl('', passValidators),
+      'newPassword' : new FormControl('', passValidators),
+      'repeatPassword' : new FormControl('', passValidators)
+    });
+  }
+
+  private initializeImageForm(){
+    this.imageForm = new FormGroup({
+      "userImage": new FormControl('', Validators.required)
+    });
   }
 
 }
