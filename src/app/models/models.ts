ad433e98454b863b200d30992cfc6dbcf7df43ed
Jagolu <javo14795@gmail.com>
2019-04-12 20:23:44 +0200

    Add createGroup model

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -3,3 +3,4 @@ export { SignUser } from './SignUser';
 export { SocialLog } from './SocialLog';
 export { ChangeUserInfo } from './ChangeUserInfo';
 export { DeleteUser } from './DeleteUser';
+export { CreateGroup } from './CreateGroup';
