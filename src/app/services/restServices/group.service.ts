9e8959a46a2c352fd04c7f868b08f9ee3493ce21
Jagolu <javo14795@gmail.com>
2019-04-12 20:27:09 +0200

    Update groupService --> add createGroup & checkGroupName

--- src/app/services/restServices/group.service.ts
+++ src/app/services/restServices/group.service.ts
@@ -2,13 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RestService } from './rest.service';
 import { LoadingService } from '../visualServices/loading.service';
+import { CreateGroup } from 'src/app/models/models';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService extends RestService{
 
+  private _groupPath : string = "Group/";
+
   constructor(http: HttpClient, loading: LoadingService) { 
     super(http, loading);
+  }
+
+  createGroup(group:CreateGroup){
+    return this.postRequest(group, this._groupPath+"CreateGroup");
+  }
+
+  checkGroupName(name:string){
+    return this.getRequest(this._groupPath+"CheckGroupName/"+name);
   }
 }
