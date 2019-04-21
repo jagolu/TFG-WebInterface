c7ffab5e9d18ce72ace2dcb6d7b4b65b236e820f
Javier Gómez Luzón <javo14795@gmail.com>
2019-04-21 10:49:11 +0200

    Change bootstrap as node_module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 // Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
