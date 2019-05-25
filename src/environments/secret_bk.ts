d173cd976f2501ed419aecd63df6c9b8ffccd600
Javier Gómez Luzón <javo14795@gmail.com>
2019-05-25 12:30:52 +0200

    Update sensitive enviroment bk

---
+++ src/environments/secret_bk.ts
@@ -0,0 +1,28 @@
+import { AuthServiceConfig, GoogleLoginProvider, FacebookLoginProvider } from 'angularx-social-login';
+
+const googleClientId = "google client-id";
+const facebookAppId = "facebook app-id";
+
+const authConfig = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider(googleClientId)
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider(facebookAppId)
+  }
+]);
+
+export const environment = {
+  production: false,
+};
+
+export function provideConfig(){
+  return authConfig;
+}
+
+export const URL = {
+  baseURL: "local_backend_url"
+  // baseURL: "cloud_backend_url"
+}
