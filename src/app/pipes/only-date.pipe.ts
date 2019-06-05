8d88090ed8be187f9b74cfd8ab1e2344089d3fba
Javier Gómez Luzón <javo14795@gmail.com>
2019-06-05 23:21:36 +0200

    Date pipe, if the day is today, shows "Today"

--- src/app/pipes/only-date.pipe.ts
+++ src/app/pipes/only-date.pipe.ts
@@ -8,9 +8,18 @@ export class OnlyDatePipe implements PipeTransform {
 
   transform(fullDate: string): string {
     try{
+      let fullYear = Number(fullDate.substring(0,4));
       let year = Number(fullDate.substring(2, 4));
       let month = Number(fullDate.substring(5, 7));
       let day = Number(fullDate.substring(8, 10));
+      let now = new Date();
+      
+      if(now.getDate() == day && 
+        now.getMonth()+1 == month && 
+        (now.getFullYear() == fullYear || now.getFullYear() == year))
+      {
+        return "Today";
+      }
   
       return `${day}/${month}/${year}`;
     }catch(Error){
