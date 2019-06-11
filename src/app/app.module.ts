4d585d0f1703dfe5e44286596c703aa6bbb861f0
Javier Gómez Luzón <javo14795@gmail.com>
2019-06-11 17:37:25 +0200

    Reorganize app module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,42 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-// Components
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { SignUpComponent } from './components/logSign/sign-up/sign-up.component';
-import { IconComponent } from './components/shared/icon/icon.component';
-import { HomeComponent } from './components/home/home.component';
-import { LogInComponent } from './components/logSign/log-in/log-in.component';
-import { EmailVerificationComponent } from './components/logSign/email-verification/email-verification.component';
-import { LoadingComponent } from './components/shared/loading/loading.component';
-import { SocialButtonComponent } from './components/logSign/social-button/social-button.component';
-import { ViewUserComponent } from './components/user/view-user/view-user.component';
-import { UserGroupsComponent } from './components/user/view-user/user-groups/user-groups.component';
-import { UserInfoFormComponent } from './components/user/view-user/user-info-form/user-info-form.component';
-import { CreateGroupAlertComponent } from './components/shared/alert/create-group-alert/create-group-alert.component';
-import { GroupComponent } from './components/group/group.component';
-import { LaunchedBetsComponent } from './components/footballBet/launched-bets/launched-bets.component';
-import { GroupUsersComponent } from './components/group/group-users/group-users.component';
-import { SearchGroupComponent } from './components/search-group/search-group.component';
-import { ShopComponent } from './components/shop/shop.component';
-import { AlertComponent } from './components/shared/alert/alert.component';
-import { DeleteAccountAlertComponent } from './components/shared/alert/delete-account-alert/delete-account-alert.component';
-import { InfoAlertComponent } from './components/shared/alert/info-alert/info-alert.component';
-import { JoinPasswordGroupComponent } from './components/shared/alert/join-password-group/join-password-group.component';
-import { GroupInfoComponent } from './components/group/group-info/group-info.component';
-import { GroupSettingsComponent } from './components/group/group-settings/group-settings.component';
-import { PasswordFormComponent } from './components/group/group-settings/password-form/password-form.component';
-import { RememberPasswordFormComponent } from './components/logSign/rememberPassword/remember-password-form/remember-password-form.component';
-import { DeleteGroupAlertComponent } from './components/shared/alert/delete-group-alert/delete-group-alert.component'
-import { ResetPasswordFormComponent } from './components/logSign/rememberPassword/reset-password-form/reset-password-form.component';
-import { SocialSignPasswordComponent } from './components/shared/alert/social-sign-password/social-sign-password.component';
-import { CreateFootballBetComponent } from './components/footballBet/create-football-bet/create-football-bet.component';
-import { DoFootballBetComponent } from './components/shared/alert/do-football-bet/do-football-bet.component';
-import { FootballBetHistoryComponent } from './components/footballBet/football-bet-history/football-bet-history.component';
-import { MyOwnFootballBetsComponent } from './components/footballBet/my-own-football-bets/my-own-football-bets.component';
-
-
 // Directives
 
 
@@ -73,6 +37,43 @@ import { provideConfig } from 'src/environments/secret';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { Base64ImagePipe } from './pipes/base64-image.pipe';
+
+
+// Components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SignUpComponent } from './components/logSign/sign-up/sign-up.component';
+import { IconComponent } from './components/shared/icon/icon.component';
+import { HomeComponent } from './components/home/home.component';
+import { LogInComponent } from './components/logSign/log-in/log-in.component';
+import { EmailVerificationComponent } from './components/logSign/email-verification/email-verification.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+import { SocialButtonComponent } from './components/logSign/social-button/social-button.component';
+import { ViewUserComponent } from './components/user/view-user/view-user.component';
+import { UserGroupsComponent } from './components/user/view-user/user-groups/user-groups.component';
+import { UserInfoFormComponent } from './components/user/view-user/user-info-form/user-info-form.component';
+import { CreateGroupAlertComponent } from './components/shared/alert/create-group-alert/create-group-alert.component';
+import { GroupComponent } from './components/group/group.component';
+import { LaunchedBetsComponent } from './components/footballBet/launched-bets/launched-bets.component';
+import { GroupUsersComponent } from './components/group/group-users/group-users.component';
+import { SearchGroupComponent } from './components/search-group/search-group.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { AlertComponent } from './components/shared/alert/alert.component';
+import { DeleteAccountAlertComponent } from './components/shared/alert/delete-account-alert/delete-account-alert.component';
+import { InfoAlertComponent } from './components/shared/alert/info-alert/info-alert.component';
+import { JoinPasswordGroupComponent } from './components/shared/alert/join-password-group/join-password-group.component';
+import { GroupInfoComponent } from './components/group/group-info/group-info.component';
+import { GroupSettingsComponent } from './components/group/group-settings/group-settings.component';
+import { PasswordFormComponent } from './components/group/group-settings/password-form/password-form.component';
+import { RememberPasswordFormComponent } from './components/logSign/rememberPassword/remember-password-form/remember-password-form.component';
+import { DeleteGroupAlertComponent } from './components/shared/alert/delete-group-alert/delete-group-alert.component'
+import { ResetPasswordFormComponent } from './components/logSign/rememberPassword/reset-password-form/reset-password-form.component';
+import { SocialSignPasswordComponent } from './components/shared/alert/social-sign-password/social-sign-password.component';
+import { CreateFootballBetComponent } from './components/footballBet/create-football-bet/create-football-bet.component';
+import { DoFootballBetComponent } from './components/shared/alert/do-football-bet/do-football-bet.component';
+import { FootballBetHistoryComponent } from './components/footballBet/football-bet-history/football-bet-history.component';
+import { MyOwnFootballBetsComponent } from './components/footballBet/my-own-football-bets/my-own-football-bets.component';
+
 
 
 @NgModule({
