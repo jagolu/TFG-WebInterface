5274624415874dc931053b356792896c61be1567
Javier Gómez Luzón <javo14795@gmail.com>
2019-06-16 14:23:53 +0200

    Show correct message on betsManager

--- src/app/models/Bets/BetsManager.ts
+++ src/app/models/Bets/BetsManager.ts
@@ -3,6 +3,7 @@ import { GroupBet } from './GroupBet';
 export interface BetsManager{
     bet:GroupBet;
     dateLaunch:Date;
+    dateEnd:Date;
     ended:boolean;
     dateCancelled?:Date;
     cancelled:boolean;
