3db00b32ecb82f7be61408219f542ac21cd48090
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-15 23:40:50 +0200

    add funcionality to cancell football bet

--- src/app/components/shared/alert/cancel-football-bet/cancel-football-bet.component.ts
+++ src/app/components/shared/alert/cancel-football-bet/cancel-football-bet.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertService } from 'src/app/services/visualServices/alert.service';
+import { BetService } from 'src/app/services/restServices/bet.service';
 
 @Component({
   selector: 'app-cancel-football-bet',
@@ -8,9 +9,18 @@ import { AlertService } from 'src/app/services/visualServices/alert.service';
 })
 export class CancelFootballBetComponent {
 
-  constructor(private alertS:AlertService) { }
+  private betId:string = "";
+
+  constructor(private alertS:AlertService, private betS:BetService) { 
+    this.alertS.target.subscribe(id=> this.betId = id);
+  }
 
   public close(){
     this.alertS.hideAlert();
+  }
+
+  public cancel(){
+    this.alertS.hideAlert();
+    this.betS.cancelFootballBet(this.betId);
   }
 }
