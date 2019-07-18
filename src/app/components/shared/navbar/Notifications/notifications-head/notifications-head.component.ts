2e0a690dc8bdba6157c95512341d13b871913fbf
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-18 22:57:58 +0200

    Fix errors

--- src/app/components/shared/navbar/Notifications/notifications-head/notifications-head.component.ts
+++ src/app/components/shared/navbar/Notifications/notifications-head/notifications-head.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { NotificationBase } from '../NotificationBase';
-import { AliveService } from 'src/app/services/restServices/alive.service';
 import { NotificationsService } from 'src/app/services/userServices/Hub/notifications.service';
+import { IconModel, Icons, NotificationMessage } from 'src/app/models/models';
 
 @Component({
   selector: 'app-notifications-head',
@@ -14,41 +13,51 @@ import { NotificationsService } from 'src/app/services/userServices/Hub/notifica
  * @class
  * @extends NotificationBase
  */
-export class NotificationsHeadComponent extends NotificationBase{
+export class NotificationsHeadComponent{
 
-  //
-  // ──────────────────────────────────────────────────────────────────────
-  //   :::::: C L A S S   V A R S : :  :   :    :     :        :          :
-  // ──────────────────────────────────────────────────────────────────────
-  //
-  
-  /**
-   * Filter to show or not the bell 
-   * when there is not unread notifications.
-   * True to always show the bell, false to hide
-   * it when there is not unread notifications.
-   * 
-   * @var {Boolean} alwaysShow
-   */
-  @Input() alwaysShow:Boolean = true;
+    //
+    // ──────────────────────────────────────────────────────────────────────
+    //   :::::: C L A S S   V A R S : :  :   :    :     :        :          :
+    // ──────────────────────────────────────────────────────────────────────
+    //  
+    /**
+     * Filter to show or not the bell 
+     * when there is not unread notifications.
+     * True to always show the bell, false to hide
+     * it when there is not unread notifications.
+     * 
+     * @var {Boolean} alwaysShow
+     */
+    @Input() alwaysShow:Boolean = true;
+    
+    /**
+     * The notifications that the user have
+     * 
+     * @access public
+     * @var {NotificationMessage[]} notifications
+     */
+    public notifications:NotificationMessage[] = [];
 
-  /**
-   * The "notifications" array and the bell_icon come
-   * from the NotificationBase class
-   */
+    /**
+     * The icon of a bell
+     * 
+     * @access public
+     * @var {IconModel} icon_bell
+     */
+    public icon_bell:IconModel = Icons.BELL;
 
-  //
-  // ──────────────────────────────────────────────────────────────────────────
-  //   :::::: C O N S T R U C T O R S : :  :   :    :     :        :          :
-  // ──────────────────────────────────────────────────────────────────────────
-  //
 
-  /**
-   * @constructor
-   * @param {AliveService} aliveS To initialize NotificationBase
-   * @param {NotificationsService} notS To initialize NotificationBase
-   */
-  constructor(aliveS:AliveService, notS:NotificationsService) { 
-    super(aliveS, notS);
-  }
+    //
+    // ──────────────────────────────────────────────────────────────────────────
+    //   :::::: C O N S T R U C T O R S : :  :   :    :     :        :          :
+    // ──────────────────────────────────────────────────────────────────────────
+    //
+    
+    /**
+     * @constructor
+     * @param {NotificationsService} notS To know the count of unread notifications
+     */
+    constructor(private notS:NotificationsService){
+        this.notS.notifications.subscribe(msgs=>this.notifications = msgs);
+    }
 }
--- src/app/components/shared/navbar/Notifications/notifications-nav/notifications-nav.component.ts
+++ src/app/components/shared/navbar/Notifications/notifications-nav/notifications-nav.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AliveService } from 'src/app/services/restServices/alive.service';
 import { NotificationsService } from 'src/app/services/userServices/Hub/notifications.service';
-import { NotificationBase } from '../NotificationBase';
+import { NotificationMessage, LoginNotification } from 'src/app/models/models';
 
 @Component({
   selector: 'app-notifications-nav',
@@ -14,25 +14,57 @@ import { NotificationBase } from '../NotificationBase';
  * @class
  * @extends NotificationBase
  */
-export class NotificationsNavComponent extends NotificationBase{
+export class NotificationsNavComponent {
 
-  /**
-   * The "notifications" array and the bell_icon come
-   * from the NotificationBase class
-   */
+    //
+    // ──────────────────────────────────────────────────────────────────────
+    //   :::::: C L A S S   V A R S : :  :   :    :     :        :          :
+    // ──────────────────────────────────────────────────────────────────────
+    //
+    
+    /**
+     * The notifications that the user have
+     * 
+     * @access public
+     * @var {NotificationMessage[]} notifications
+     */
+    public notifications:NotificationMessage[] = [];
 
-  //
-  // ──────────────────────────────────────────────────────────────────────────
-  //   :::::: C O N S T R U C T O R S : :  :   :    :     :        :          :
-  // ──────────────────────────────────────────────────────────────────────────
-  //
 
-  /**
-   * @constructor
-   * @param {AliveService} aliveS To initialize NotificationBase
-   * @param {NotificationsService} notS To initialize NotificationBase
-   */
-  constructor(aliveS:AliveService, notS:NotificationsService) { 
-    super(aliveS, notS);
-  }
+    //
+    // ──────────────────────────────────────────────────────────────────────────
+    //   :::::: C O N S T R U C T O R S : :  :   :    :     :        :          :
+    // ──────────────────────────────────────────────────────────────────────────
+    //
+    
+    /**
+     * @constructor
+     * @param {AliveService} aliveS To get the unread notifications of the user
+     * @param {NotificationsService} notS To get the new incoming notifications
+     */
+    constructor(private aliveS:AliveService, private notS:NotificationsService){
+        this.aliveS.getNotifications().subscribe((n:LoginNotification)=>
+            this.notS.initialize(n.publicUserid, n.messages));
+
+        this.notS.notifications.subscribe(msgs=>this.notifications = msgs);
+    }
+
+    //
+    // ──────────────────────────────────────────────────────────────────────────────────
+    //   :::::: P U B L I C   F U N C T I O N S : :  :   :    :     :        :          :
+    // ──────────────────────────────────────────────────────────────────────────────────
+    //
+
+    /**
+     * Mark one notification as "Read" and delete it
+     * 
+     * @access public
+     * @param {NotificationMessage} not The notification to read
+     */
+    public watchNotification(not:NotificationMessage){
+        this.aliveS.readNotification(not.id).subscribe(_=>{
+            let index = this.notifications.indexOf(not, 0);
+            if(index>-1) this.notifications.splice(index, 1);
+        });
+    }
 }
