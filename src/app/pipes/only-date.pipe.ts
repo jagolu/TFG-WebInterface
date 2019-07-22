91a699db48a64ad5c6678417c4e7c6054469d785
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-22 20:50:31 +0200

    Update chatTime & onlyDate pipes

--- src/app/pipes/only-date.pipe.ts
+++ src/app/pipes/only-date.pipe.ts
@@ -6,19 +6,34 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class OnlyDatePipe implements PipeTransform {
 
+  /**
+   * Returns a string with a formatted date
+   * 
+   * @param {string} fullDate Datetime format
+   * @return {string} 
+   */
   transform(fullDate: string): string {
     try{
+      //The actual year format 'YYYY'
       let fullYear = Number(fullDate.substring(0,4));
+
+      //The actual year format 'YY'
       let year = Number(fullDate.substring(2, 4));
+
+      //The actual month format 'MM'
       let month = Number(fullDate.substring(5, 7));
+
+      //The actual day format 'DD'
       let day = Number(fullDate.substring(8, 10));
+
+      //The actual datetime right now
       let now = new Date();
       
-      if(now.getDate() == day && 
-        now.getMonth()+1 == month && 
+      //If is today, just return "Hoy"
+      if(now.getDate() == day && now.getMonth()+1 == month && 
         (now.getFullYear() == fullYear || now.getFullYear() == year))
       {
-        return "Today";
+        return "Hoy";
       }
   
       return `${day}/${month}/${year}`;
