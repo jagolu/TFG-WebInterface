ac1abc0ac80ecc977dd73ea270b9800d41a3d278
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-25 20:05:26 +0200

    Update allConversations

--- src/app/components/direct-messages/all-conversations/all-conversations.component.ts
+++ src/app/components/direct-messages/all-conversations/all-conversations.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { DirectMessagesService } from 'src/app/services/restServices/direct-messages.service';
 import { DMTitle, SearchUserDM } from 'src/app/models/models';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, Validator } from '@angular/forms';
 import { SessionService } from 'src/app/services/userServices/session.service';
 import { AdminService } from 'src/app/services/restServices/admin.service';
 
@@ -10,47 +10,125 @@ import { AdminService } from 'src/app/services/restServices/admin.service';
   templateUrl: './all-conversations.component.html',
   styles: [`.closedConv{background-color:#F2F2F2}`]
 })
-export class AllConversationsComponent  {
-  public dmTitles:DMTitle[] = [];
-  public suggestions:SearchUserDM[] = [];
-  public validSelect:boolean = false;
-  private createDMForm:FormGroup;
+export class AllConversationsComponent {
 
-  constructor(private _dmS:DirectMessagesService, private sessionS:SessionService, private adminS:AdminService) { 
-    this._dmS.loadDMTitles().subscribe((dmS:DMTitle[])=> this.dmTitles = dmS);
+  //
+  // ──────────────────────────────────────────────────────────────────────
+  //   :::::: C L A S S   V A R S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * The direct conversations of the
+   * current user
+   * 
+   * @access public
+   * @var {DMTitle[]} dmTitles
+   */
+  public dmTitles:DMTitle[] = [];
+
+  /**
+   * (For admin users) The result user-search
+   * 
+   * @access public
+   * @var {SearchUserDM[]} suggestions
+   */
+  public suggestions:SearchUserDM[] = [];
+
+  /**
+   * The form to create a Direct Conversation
+   * 
+   * @access public
+   * @var {FormGroup} createDMForm
+   */
+  public createDMForm:FormGroup;
+
+
+  //
+  // ──────────────────────────────────────────────────────────────────────────
+  //   :::::: C O N S T R U C T O R S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * Load the direct conversations
+   * 
+   * @constructor
+   * @param {DirectMessagesService} __dmS To do the direct messages requests
+   * @param {SessionService} __sessionS To know if the current user is an admin or not
+   * @param {AdminService} __adminS To search the users
+   */
+  constructor(private __dmS:DirectMessagesService, private __sessionS:SessionService, private __adminS:AdminService) { 
+    this.__dmS.loadDMTitles().subscribe((dmS:DMTitle[])=> this.dmTitles = dmS);
     this.initializeForm();
-    this.validSelect = this.isAdmin() ? false : true;
   }
 
+
+  //
+  // ──────────────────────────────────────────────────────────────────────────────────
+  //   :::::: P U B L I C   F U N C T I O N S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * Creates a new direct conversation
+   * 
+   * @access public
+   */
   public createDMTitle(){
     let title = this.createDMForm.controls["title"].value;
-    let receiver = this.isAdmin() ? (document.querySelector("#selectSuggsUserIdDMSearch") as HTMLSelectElement).value : null;
-    this._dmS.launchDMTitle({
+    let receiver = this.isAdmin() ? this.createDMForm.controls["receiver"].value : null;
+    this.__dmS.launchDMTitle({
       "title": title,
       "emailReceiver": receiver
-    }).subscribe((dmS:DMTitle[])=>{
-      this.dmTitles = dmS;
-      this.resetForm();
-    });
+    }).subscribe((dmS:DMTitle[])=> this.dmTitles = dmS);
+    this.resetForm();
   }
 
-  public changeInputSelect(){
-    let index = (document.querySelector("#selectSuggsUserIdDMSearch") as HTMLSelectElement).selectedIndex;
-    this.changeSelectIndex(index);
+  /**
+   * Says if the current user is an admin or not
+   * 
+   * @access public
+   * @returns {Boolean} True if the current user
+   * is an admin, false otherwise
+   */
+  public isAdmin():Boolean{
+    return this.__sessionS.isAdmin();
   }
 
-  public isAdmin(){
-    return this.sessionS.isAdmin();
-  }
-
+  /**
+   * Ajax function to search a user (Only for admins) and
+   * change the selected index to 0
+   * 
+   * @access public
+   */
   public findDM(){
-    this.changeSelectIndex(0);
-    let find = (document.querySelector("#findUserDMId") as HTMLInputElement).value;
-    this.adminS.searchUserDM(find).subscribe((suggs:SearchUserDM[])=>this.suggestions = suggs);
+    let find = this.createDMForm.controls["receiver"].value;
+    this.__adminS.searchUserDM(find).subscribe((suggs:SearchUserDM[])=>this.suggestions = suggs);
   }
 
+
+  //
+  // ────────────────────────────────────────────────────────────────────────────────────
+  //   :::::: P R I V A T E   F U N C T I O N S : :  :   :    :     :        :          :
+  // ────────────────────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * Initializes the form to create 
+   * a new direct conversation
+   * 
+   * @access private
+   */
   private initializeForm(){
+    let recvValidator = this.isAdmin() ? Validators.required : Validators.nullValidator;
     this.createDMForm = new FormGroup({
+      "receiver": new FormControl(
+        '',
+        [
+          recvValidator
+        ]
+      ),
       "title": new FormControl(
         '',
         [
@@ -62,15 +140,13 @@ export class AllConversationsComponent  {
     });
   }
 
+  /**
+   * Resets the form and the select input
+   * 
+   * @access private
+   */
   private resetForm(){
-    this.createDMForm.reset({"title": ""});
-    (document.querySelector("#findUserDMId") as HTMLInputElement).value = "";
-    this.changeSelectIndex(0);
+    this.createDMForm.reset({"title": "", "receiver": ""});
     this.suggestions = [];
-  }
-
-  private changeSelectIndex(index:number){
-    (document.querySelector("#selectSuggsUserIdDMSearch") as HTMLSelectElement).selectedIndex = index;
-    this.validSelect = index!=0;
   }
 }
