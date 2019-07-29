c303517bcce4aaa0cf2d3f1739c4ccbc57b0bb34
Javier Gómez Luzón <javo14795@gmail.com>
2019-07-30 00:18:39 +0200

    Update football bet card info

--- src/app/components/footballBet/card-info/football-bet-card-info/football-bet-card-info.component.ts
+++ src/app/components/footballBet/card-info/football-bet-card-info/football-bet-card-info.component.ts
@@ -9,17 +9,72 @@ import { AlertService } from 'src/app/services/visualServices/alert.service';
 })
 export class FootballBetCardInfoComponent{
 
+  //
+  // ──────────────────────────────────────────────────────────────────────
+  //   :::::: C L A S S   V A R S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * The info of the bet
+   * 
+   * @access public
+   * @var {GroupBet} bet
+   */
   @Input() bet: GroupBet;
+
+  /**
+   * The id of the bet
+   * 
+   * @access public
+   * @var {string} betId
+   */
   @Input() betId:string = "";
+
+  /**
+   * Says if the bet is ended or not
+   * 
+   * @access public
+   * @var {Boolean} ended
+   */
   @Input() ended:Boolean = true;
 
-  constructor(private alertS:AlertService) { }
 
-  public isJackpotBet(type:string):boolean{
+  //
+  // ──────────────────────────────────────────────────────────────────────────
+  //   :::::: C O N S T R U C T O R S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * @constructor
+   * @param {AlertService} __alertS To launch the alert to cancel the bet
+   */
+  constructor(private __alertS:AlertService) { }
+
+
+  //
+  // ────────────────────────────────────────────────────────────────────────  ──────────
+  //   :::::: P U B L I C   F U N C T I O N S : :  :   :    :     :        :          :
+  // ──────────────────────────────────────────────────────────────────────────────────
+  //
+
+  /**
+   * Says if the bet is a jackpot bet
+   * 
+   * @access public
+   * @param {Boolean} type The type of the bet
+   */
+  public isJackpotBet(type:string):Boolean{
     return type.includes("JACKPOT");
   }
 
+  /**
+   * Launchs the alert to cancel the bet
+   * 
+   * @access public
+   */
   public cancel(){
-    this.alertS.cancelFootballBet(this.betId);
+    this.__alertS.cancelFootballBet(this.betId);
   }
 }
